1fdb6345976e2a9521af3b51ed9ae35bcd8cc4da
vernono <60306077+vernonOber@users.noreply.github.com>
2021-01-03 14:12:01 -0500

    final for packaging on PyPi

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import setuptools
 
 
 setuptools.setup(
-    name='vavista-rpc-VernonO',
-    version='0.0.9',
+    name='vavista_rpc',
+    version='0.0.10',
     author='Conor Dowling, Kevin Gill',
     author_email='vo2521@gmail.com',
     description='VAVista RPC interface',
